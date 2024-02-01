46e25d56bc0477bd098a9b225b09ae083348c827
Junxiang Xu <chucksailors95@gmail.com>
2024-02-01 13:34:41 -0500

    rename

--- R/class_FEA.R
+++ R/class_FEA.R
@@ -2,7 +2,7 @@
 #'
 #' @slot id character. 
 #' @slot dataset_id character. 
-#' @slot feature_name character. 
+#' @slot feature_modality character. 
 #' @slot reference_database character. 
 #' @slot reference_organism character. 
 #' @slot record_id character. 
@@ -15,7 +15,7 @@ setClass("FEA", slots = list(id = "character",
                              dataset_id = "character",
                              filepath = "character",
                              accessor = "character",
-                             feature_name = "character",
+                             feature_modality = "character",
                              reference_database = "character",
                              reference_organism = "character",
                              record_id = "character"))
@@ -25,7 +25,7 @@ create_FEA_object <- function(
     dataset_id = NA_character_,    
     filepath = NA_character_,
     accessor = NA_character_,
-    feature_name = NA_character_,
+    feature_modality = NA_character_,
     reference_database = NA_character_,
     reference_organism = NA_character_,
     record_id = NA_character_
@@ -35,7 +35,7 @@ create_FEA_object <- function(
              dataset_id = dataset_id,
              accessor = accessor,
              data_type = data_type,
-             feature_name = feature_name,
+             feature_modality = feature_modality,
              reference_database = reference_database,
              reference_organism = reference_organism,
              record_id = record_id
@@ -67,9 +67,9 @@ setMethod("accessor<-", "FEA", function(x, value) {
   x 
 })
 
-setMethod("feature_name", "FEA", function(x) x@feature_name)
-setMethod("feature_name<-", "FEA", function(x, value) {
-  x@feature_name <- value
+setMethod("feature_modality", "FEA", function(x) x@feature_modality)
+setMethod("feature_modality<-", "FEA", function(x, value) {
+  x@feature_modality <- value
   x
 })
 
