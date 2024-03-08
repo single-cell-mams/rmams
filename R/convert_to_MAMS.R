b2d09e31608a787bf3e8079ae244c8442f1ae8d7
irzamsarfraz <irzam9095@gmail.com>
2024-03-08 13:57:16 -0500

    fixed a bug

--- R/convert_to_MAMS.R
+++ R/convert_to_MAMS.R
@@ -43,18 +43,23 @@ convert_seurat_to_MAMS <- function(object_list,observation_subsets){
                     representation <- "sparse"
                     processing <- "counts"
                     feature_subset <- "full"
+                    MAMS@FOM[[fom]] <- create_FOM_object(id = fom, filepath=filepath, accessor=accessor, oid=oid, processing=processing, modality=modality, analyte=analyte)
+                    
                 }else if(assay == "data"){
                     data_type <- "double"
                     representation <- "sparse"
                     processing <- "lognormalized"
                     feature_subset <- "full"
+                    MAMS@FOM[[fom]] <- create_FOM_object(id = fom, filepath=filepath, accessor=accessor, oid=oid, processing=processing, modality=modality, analyte=analyte)
+                    
                 }else if(assay == "scale.data"){
                     data_type <- "double"
                     representation <- "dense"
                     processing <- "scaled"
                     feature_subset <- "variable"
+                    MAMS@FOM[[fom]] <- create_FOM_object(id = fom, filepath=filepath, accessor=accessor, oid=oid, processing=processing, modality=modality, analyte=analyte)
+                    
                 }
-                MAMS@FOM[[fom]] <- create_FOM_object(id = fom, filepath=filepath, accessor=accessor, oid=oid, processing=processing, modality=modality, analyte=analyte)
               #  FOMs[[fom]] <- create_FOM_object(id = fom, filepath = filepath, accessor = accessor, representation = representation, analyte = analyte, modality = modality, obs_subset = obs_subset, feature_subset = feature_subset, oid = oid, fid = fid, obs = obs, fea = fea)
             }
         }
