6b447168881c1abefa02e54e55185f8d6292946d
Irzam Sarfraz <33160268+irzamsarfraz@users.noreply.github.com>
2024-05-10 20:09:42 -0400

    Update test-tests.R

--- tests/testthat/test-tests.R
+++ tests/testthat/test-tests.R
@@ -13,31 +13,23 @@ test_that("Test MAMS functions", {
     
     # Test convert_seurat_to_MAMS
     mams <- convert_seurat_to_MAMS(object_list = list(srt = srt, subset_srt = subset_srt), 
-                                   observation_subsets = c("full", "subset"))
+                                   observation_subsets = c("full", "subset"), dataset_id = "dataset1")
     # test if its a mam object
     expect_is(mams, "MAMS")
     # test if first fom is counts
-    # expect_equal(mams@FOM$fom1@processing, "counts")
+    expect_equal(mams@FOM$fom1@processing, "counts")
     # test if third fom is scaled
-    # expect_equal(mams@FOM$fom3@processing, "scaled")
+    expect_equal(mams@FOM$fom3@processing, "scaled")
     
     
     # Test check_MAMS
-    # fom(mams, "fom1", "dataset_id") <- "1"
-    # fom(mams, "fom2", "dataset_id") <- "1"
-    # fom(mams, "fom3", "dataset_id") <- "1"
-    # fom(mams, "fom4", "dataset_id") <- "1"
-    # fom(mams, "fom5", "dataset_id") <- "1"
-    # fom(mams, "fom6", "dataset_id") <- "1"
-    # fom(mams, "fom1", "data_type") <- "dt"
-    # fom(mams, "fom2", "data_type") <- "dt"
-    # fom(mams, "fom3", "data_type") <- "dt"
-    # fom(mams, "fom4", "data_type") <- "dt"
-    # fom(mams, "fom5", "data_type") <- "dt"
-    # fom(mams, "fom6", "data_type") <- "dt"
-    
-    # expect_error(check_MAMS(mams), 'fom6(ID:fom6) is missing the field(s): c("dataset_id", "data_type")')
-    
+    fom(mams, "fom1", "data_type") <- "dt1"
+    fom(mams, "fom2", "data_type") <- "dt2"
+    fom(mams, "fom3", "data_type") <- "dt3"
+    fom(mams, "fom4", "data_type") <- "dt4"
+    fom(mams, "fom5", "data_type") <- "dt5"
+    fom(mams, "fom6", "data_type") <- "dt6"
+    expect_equal(mams@FOM$fom6@data_type, "dt6")
     
     # Test export to JSON
     
